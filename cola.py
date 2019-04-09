ade0a1f91a8bc9fc7819fdd1669270cc0b84378e
daniels0009 <36423740+daniels0009@users.noreply.github.com>
2019-04-09 00:53:31 -0500

    Update cola.py

--- cola.py
+++ cola.py
@@ -1,7 +1,7 @@
 clase  Cola :
     "" " Representa una cola con operaciones de encolar, desencolar y
         verificar si está vacía. ""
-
+ 
     def  __init__ ( self ):
         "" " Crea una cola vacía. " ""
         # La cola vacía se representa con una lista vacía
@@ -16,16 +16,10 @@ clase  Cola :
         "" Devuelve el elemento inicial y lo elimina de la cola.
             Si la cola está vacía levanta una excepción. ""
         tratar :
-            atendido =  auto .items [ 0 ]
-            auto .items.pop ( 0 )
-            volver atendido
+            volver  auto .items.pop ( 0 )
         excepto  IndexError :
-            impresión
-            " La cola esta vacia "
+            elevar  ValueError ( " La cola está vacía " )
 
     def  es_vacia ( self ):
         "" " Devuelve True si la lista está vacía, False si no. " ""
         volver  auto .items == []
-
-    def  tamano ( uno mismo ):
-        volver  len ( auto .items)
